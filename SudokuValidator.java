f690b668320bfe5ff54fdee7951de4352d4d8e0c
Harsh Agarwal <130899605+agrwalh@users.noreply.github.com>
2026-03-29 21:51:21 +0530

    Update SudokuValidator.java

--- SudokuValidator.java
+++ SudokuValidator.java
@@ -26,10 +26,30 @@ public class SudokuValidator {
 
 			if (choice == 1) {
 
-				int[][] grid = { { 5, 3, 4, 6, 7, 8, 9, 1, 2 }, { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
-						{ 1, 9, 8, 3, 4, 2, 5, 6, 7 }, { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
-						{ 7, 1, 3, 9, 2, 4, 8, 5, 6 }, { 9, 6, 1, 5, 3, 7, 2, 8, 4 }, { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
-						{ 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
+				int[][] grid = new int[9][9];
+
+				System.out.println("Enter Sudoku (9x9 grid, values 1-9):");
+
+				for (int i = 0; i < 9; i++) {
+					for (int j = 0; j < 9; j++) {
+
+						if (scanner.hasNextInt()) {
+							int val = scanner.nextInt();
+
+							if (val >= 1 && val <= 9) {
+								grid[i][j] = val;
+							} else {
+								System.out.println("Invalid input! Enter numbers between 1-9.");
+								j--; // same position dobara fill karwao
+							}
+
+						} else {
+							System.out.println("Invalid input! Enter only numbers.");
+							scanner.next();
+							j--; // retry same cell
+						}
+					}
+				}
 
 				try {
 					SudokuUtil.validateStructure(grid);
@@ -147,7 +167,7 @@ public class SudokuValidator {
 			}
 
 			else if (choice == 3) {
-				System.out.println("Exit");
+				System.out.println("Exited");
 				break;
 			}
 
